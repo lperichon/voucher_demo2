c656ee11daae0f55db0a287c47017151c69fa4b6
Luis Perichon <luigi@luigi-laptop.(none)>
2010-12-13 17:08:26 -0300

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VoucherDemo2::Application.config.secret_token = '1b69d7c5fbee1aa310a989455ce28fcc32bafcba34be74c8af1286f5fe4ad16fafd563cab94e0afb2e5eaeabec7d9c22a79d771c32eea471231a80364b1ec83a'
