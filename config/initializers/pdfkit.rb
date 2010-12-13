b8cb2e5cbeaba7493657811c5fecd72f70ea2806
Luis Perichon <luigi@luigi-laptop.(none)>
2010-12-13 18:24:10 -0300

    moved to bin, as it says here: https://github.com/jordan-brough/heroku-pdf/#readme

--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -1,3 +1,3 @@
 PDFKit.configure do |config|
-  config.wkhtmltopdf = Rails.root.join('vendor', 'wkhtmltopdf-amd64').to_s if Rails.env == 'production'
+  config.wkhtmltopdf = Rails.root.join('bin', 'wkhtmltopdf-amd64').to_s if Rails.env == 'production'
 end
